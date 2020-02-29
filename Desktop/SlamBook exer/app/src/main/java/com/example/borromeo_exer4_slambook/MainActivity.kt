9791ab4407bd0bb33d46f328786c2eaaabf1d7f6
Daniel William Borromeo <drborromeo@up.edu.ph>
2020-02-29 11:10:06 +0800

    chore: created bindgin object

--- Desktop/SlamBook exer/app/src/main/java/com/example/borromeo_exer4_slambook/MainActivity.kt
+++ Desktop/SlamBook exer/app/src/main/java/com/example/borromeo_exer4_slambook/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.borromeo_exer4_slambook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.borromeo_exer4_slambook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
